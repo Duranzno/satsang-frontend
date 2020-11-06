03ee77706a0ed5f9d3fa85e483c839062aa190c8
Alejandro D <aledurax@gmail.com>
2020-11-06 11:42:17 -0400

    feat(test.yml): add Basic Testing Github Action

--- ignore-build-step.sh
+++ ignore-build-step.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Retrieve branch name
-branch=$(git branch --show-current)
+# # Retrieve branch name
+# branch=$(git branch --show-current)
 
-if [ "$branch" == "deploy" ]; then
-  # Proceed with build
-    exit 1;
-else
-  # Don't build
-  exit 0;
-fi
+# if [ "$branch" == "deploy" ]; then
+#   # Proceed with build
+#     exit 1;
+# else
+#   # Don't build
+#   exit 0;
+# fi
